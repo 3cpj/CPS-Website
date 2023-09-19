49e90a94bd691115ade16d8a03147ea1d750a3c3
Aaron <87758101+3cpj@users.noreply.github.com>
2023-09-20 03:53:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cpsumsu.org',
-      },
-      {
-        protocol: 'https',
-        hostname: process.env.IMAGE_HOSTNAME,
-      }
-    ]
-  }
-}
-
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */  
+const nextConfig = {  
+  reactStrictMode: true,  
+  images: {  
+    remotePatterns: [  
+      {  
+        protocol: 'https',  
+        hostname: 'cpsumsu.org',  
+        port: '',  
+        pathname: '/path/to/images/**'  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: process.env.IMAGE_HOSTNAME,  
+        port: '',  
+        pathname: '/path/to/images/**'  
+      }  
+    ]  
+  }  
+}  
+  
+module.exports = nextConfig;
